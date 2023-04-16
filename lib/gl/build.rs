5ca97b710d78b734bad016889108e14f64e499c8
platc2 <casimir.benjamin.platzer@alumni.bfh.ch>
2023-04-16 19:02:47 -0400

    Update OpenGL extensions (#5)
    - Add OpenGL extension for 4.2 features in shader code
    - Add OpenGL extension for compute shaders

--- lib/gl/build.rs
+++ lib/gl/build.rs
@@ -10,7 +10,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let mut file_gl = File::create(&Path::new(&out_dir).join("bindings.rs")).unwrap();
 
-    let extensions = ["GL_NV_command_list"];
+    let extensions = ["GL_ARB_compute_shader", "GL_ARB_shading_language_420pack"];
 
     Registry::new(Api::Gl, (4, 5), Profile::Core, Fallbacks::All, extensions)
         .write_bindings(GlobalGenerator, &mut file_gl)
